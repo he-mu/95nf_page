9a92296c7dd07c067d20e2c27b042a43787bc7dc
hemu <heikki.saarelma@gmail.com>
2015-08-23 18:20:37 +0300

    Web page added

---
+++ js/95nf.js
@@ -0,0 +1,10 @@
+function removeCurtain() {
+    var varCurtain = document.getElementById('pagemaker');
+    varCurtain.classList.toggle('curtain_fade')
+}
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
